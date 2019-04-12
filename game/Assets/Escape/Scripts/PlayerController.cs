29d694c1aca4c5f0deea0f9c5206393739b114ff
skjobs3 <skjobs3@gmail.com>
2019-04-12 12:35:36 +0300

    Add image placeholder

--- game/Assets/Escape/Scripts/PlayerController.cs
+++ game/Assets/Escape/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using XInputDotNetPure;
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerIndex Index;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +16,5 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GamePadState state = GamePad.GetState(PlayerIndex.One);
-
-        if(state.Buttons.A == ButtonState.Pressed)
-        {
-            Debug.Log("a1");
-        }
-
-        state = GamePad.GetState(PlayerIndex.Two);
-
-        if (state.Buttons.A == ButtonState.Pressed)
-        {
-            Debug.Log("a2");
-        }
     }
 }
