a55dfc6750cd91e18ee00bbc06306a9f4b1b8f58
Dmytro Magilin <dmitrymagilin@gmail.com>
2019-04-12 18:48:06 +0300

    Create moved background with stars

--- game/Assets/Fly/Space/UI/MoveBackground.cs
+++ game/Assets/Fly/Space/UI/MoveBackground.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveBackground : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,20 @@ public class MoveBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        var renderer = gameObject.GetComponent<Renderer>();
+        if (!renderer)
+        {
+
+            return;
+        }
+
+        var material = renderer.materials[0];
+        if (!material)
+        {
+            return;
+        }
+
+        material.SetTextureOffset("_MainTex", material.GetTextureOffset("_MainTex") + new Vector2(0.01f * speed, 0));
     }
 }
