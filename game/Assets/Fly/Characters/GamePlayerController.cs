6a640180698376c97a49908c7bcac6f3d5375433
vladyslav.reviakin <vladyslav.reviakin@ubisoft.com>
2019-04-12 14:19:52 +0300

    Added basic character, + interactive layer

---
+++ game/Assets/Fly/Characters/GamePlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class GamePlayerController : MonoBehaviour
+{
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerIndex playerIndex = PlayerIndex.One;
+        if (index == 1)
+        {
+            playerIndex = PlayerIndex.Two;
+        }
+        GamePadState state = GamePad.GetState(playerIndex);
+       
+        if (state.IsConnected)
+        {
+            var pos = this.gameObject.transform.position;
+            pos.x += state.ThumbSticks.Left.X / 10.0f;
+            pos.y += state.ThumbSticks.Left.Y / 10.0f;
+            this.gameObject.transform.position = pos;
+        }
+    }
+}
