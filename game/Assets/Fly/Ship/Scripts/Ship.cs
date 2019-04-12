de064c6ea13fd4c217bc18701ed10e1c1037c44a
dmitriy.zhebynev <dmitriy.zhebynev@ubisoft.com>
2019-04-12 14:28:48 +0300

    [Fly.Ship] physics update

---
+++ game/Assets/Fly/Ship/Scripts/Ship.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField]
+    public int DurabilityCurrent;
+
+    [SerializeField]
+    public int DurabilityMax;
+
+    public int SpeedBoost;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
