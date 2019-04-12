b92699665dc2283d0dfa75cfb535a99c9d419a13
skjobs3 <skjobs3@gmail.com>
2019-04-12 17:44:10 +0300

    Add new tags and initial impl of supplies

--- game/Assets/Escape/Scripts/PlayerController.cs
+++ game/Assets/Escape/Scripts/PlayerController.cs
@@ -7,15 +7,45 @@ public class PlayerController : MonoBehaviour
 {
     public float MovementSpeed;
     public PlayerIndex Index;
+
     public GameObject FireBehaviorPrefab;
     public GameObject CarryBehaviorPrefab;
-   
-    private GameObject m_behavior;
+    public GameObject AmmoSupplyPrefab;
+    public GameObject FuelSuppltPrefab;
+
+    public List<SupplyZone> m_supplyZones;
+
+    public bool IsAbleToTakeSupply
+    {
+        get
+        {
+            return m_gun.activeSelf;
+        }
+    }
+
+    public bool IsCarryingSupply
+    {
+        get
+        {
+            //#TODO:
+            return false;
+        }
+    }
+
+    private GameObject m_gun;
 
     // Start is called before the first frame update
     void Start()
     {
-        SetFireBehavior();
+        Transform socket = transform.Find("FireBehaviorSocket");
+        m_gun = Instantiate(FireBehaviorPrefab, socket);
+
+        PlayeFireBehavior gun = m_gun.GetComponent<PlayeFireBehavior>();
+        gun.Index = Index;
+        gun.FireRate = 10;
+        gun.RotationSpeed = 290.0f;
+
+        SetGunEnabled(true);
     }
 
     // Update is called once per frame
@@ -29,6 +59,21 @@ public class PlayerController : MonoBehaviour
         }
 
         ProcessMovement(state);
+
+        if(state.Buttons.A == ButtonState.Pressed)
+        {
+            if(IsAbleToTakeSupply && !TakeSupplyFromTheGound())
+            {
+                TakeSupplyFromZone();
+            }
+        }
+        else
+        {
+            if(IsCarryingSupply)
+            {
+                DropSupply();
+            }
+        }
     }
 
     void ProcessMovement(GamePadState state)
@@ -41,19 +86,54 @@ public class PlayerController : MonoBehaviour
         gameObject.transform.position = pos;
     }
 
-    void SetFireBehavior()
+    void SetGunEnabled(bool enabled)
     {
-        Transform socket = transform.Find("FireBehaviorSocket");
-        m_behavior = Instantiate(FireBehaviorPrefab, socket);
-
-        PlayeFireBehavior gun = m_behavior.GetComponent<PlayeFireBehavior>();
-        gun.Index = Index;
-        gun.FireRate = 10;
-        gun.RotationSpeed = 290.0f; 
+        m_gun.SetActive(enabled);
     }
 
-    void SetCarryBehavior()
+    bool TakeSupplyFromZone()
     {
+        foreach(SupplyZone zone in m_supplyZones)
+        {
+            var playerCollider = gameObject.GetComponent<Collider2D>();
 
+            if(zone.GetComponent<Collider2D>().IsTouching(playerCollider))
+            {
+                Debug.Log("Taking zone supply");
+                //#TODO: Take supply from this zone
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool TakeSupplyFromTheGound()
+    {
+        var supplies = GameObject.FindGameObjectsWithTag("supply");
+
+        if (supplies.Length == 0)
+        {
+            return false;
+        }
+
+        foreach(GameObject supply in supplies)
+        {
+            var playerCollider = gameObject.GetComponent<Collider2D>();
+
+            if (supply.GetComponent<Collider2D>().IsTouching(playerCollider))
+            {
+                Debug.Log("Pickuing up supply from ground");
+                //#TODO: Pickup this supply
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void DropSupply()
+    {
+        //#TODO:
     }
 }
