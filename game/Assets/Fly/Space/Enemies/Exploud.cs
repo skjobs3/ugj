d96244c1622f058d3d041102e64a1492fa8160c7
Dmytro Magilin <dmitrymagilin@gmail.com>
2019-04-12 22:02:15 +0300

    Made garbage generation for enimies

--- game/Assets/Fly/Space/Enemies/Exploud.cs
+++ game/Assets/Fly/Space/Enemies/Exploud.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Exploud : MonoBehaviour
 {
+    public float Power = 50f;
     public GameObject[] garbageObjects;
     // Start is called before the first frame update
     void Start()
@@ -17,16 +18,15 @@ public class Exploud : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision otherObj)
+    void OnCollisionEnter2D(Collision2D otherObj)
     {
-        Debug.Log("AAAAAAAAA");
         Random rand = new Random();
-        foreach (GameObject garbObj in garbageObjects)
+        foreach (GameObject garbObjType in garbageObjects)
         {
-            Instantiate(garbObj, this.transform);
-            var force = UnityEngine.Random.insideUnitCircle * 5;
-            var rigitBody = garbObj.GetComponent<Rigidbody2D>();
-            rigitBody.AddForce(force, ForceMode2D.Impulse);
+            var GO = Instantiate(garbObjType, this.transform.position, Quaternion.identity);
+            var velocity = UnityEngine.Random.insideUnitCircle * Power;
+            var rigitBody = GO.GetComponent<Rigidbody2D>();
+            rigitBody.velocity = velocity;
         }
 
         Destroy(gameObject);
---
+++ game/Assets/Fly/Space/Enemies/LifeTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour
+{
+    public float lifeTime = 1.0f;
+
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer > (lifeTime))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
