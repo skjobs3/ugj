35701096163c071b402e18a7e2ae218b840d52e1
skjobs3 <skjobs3@gmail.com>
2019-04-13 02:57:36 +0300

    Add full implementation of all necessary gameplay actors

---
+++ game/Assets/Escape/Scripts/RulesUIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RulesUIController : MonoBehaviour
+{
+    public Text AmmoText;
+    public Text FuelText;
+    public Text TimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateAmmo(int current, int total)
+    {
+        AmmoText.text = current.ToString() + "/" + total.ToString();
+    }
+
+    public void UpdateFuel(int current, int total)
+    {
+        FuelText.text = current.ToString() + "/" + total.ToString();
+    }
+
+    public void UpdateTimer(int current)
+    {
+        TimerText.text = current.ToString();
+    }
+}
