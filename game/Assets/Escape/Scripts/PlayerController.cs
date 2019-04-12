029766b4e47a5d2bd2f18ae08aadfecab64efce4
skjobs3 <skjobs3@gmail.com>
2019-04-12 13:53:12 +0300

    Add basic player movement

--- game/Assets/Escape/Scripts/PlayerController.cs
+++ game/Assets/Escape/Scripts/PlayerController.cs
@@ -5,16 +5,41 @@ using XInputDotNetPure;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
     public PlayerIndex Index;
+    public GameObject GunPrefab;
+
+    private GameObject m_gun;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_gun = Instantiate(GunPrefab, transform);
+        m_gun.GetComponent<PlayerGunController>().Index = Index;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GamePadState state = GamePad.GetState(Index);
+
+        if(!state.IsConnected)
+        {
+            return;
+        }
+
+        ProcessMovement(state);
+    }
+
+    void ProcessMovement(GamePadState state)
+    {
+        Vector3 leftStick = new Vector3(state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y, 0.0f);
+        Vector3 rightStick = new Vector3(state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y, 0.0f);
+
+        Debug.Log("X:" + leftStick.x.ToString() + " Y:" + leftStick.y.ToString());
+
+        Vector3 pos = gameObject.transform.position;
+        pos += leftStick * Speed * Time.deltaTime;
+        gameObject.transform.position = pos;
     }
 }
---
+++ game/Assets/Escape/Scripts/PlayerGunController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class PlayerGunController : MonoBehaviour
+{
+    public PlayerIndex Index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GamePadState state = GamePad.GetState(Index);
+
+        if (!state.IsConnected)
+        {
+            return;
+        }
+
+        Vector3 stick = new Vector3(state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y, 0.0f);
+
+        if (stick != Vector3.zero)
+        {
+            stick *= -1.0f;
+
+            float angle = Mathf.Atan2(stick.y, stick.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
